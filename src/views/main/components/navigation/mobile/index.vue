ca7f564d74655b336eb9eeb8f3582e6e1e816cb5
Aybuai <13624448268@163.com>
2025-07-14 11:35:21 +0800

    feat: 增加滑块

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="bg-white sticky top-0 left-0 z-10">
     <ul
+      ref="ulTarget"
       class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
     >
+      <!-- 滑块 -->
+      <li
+        ref="sliderTarget"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+        :style="sliderStyle"
+      ></li>
+
       <!-- 汉堡按钮 -->
       <li
         class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
@@ -10,10 +18,16 @@
         <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
       </li>
 
+      <!-- items -->
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
         :key="item.id"
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :class="{
+          'text-zinc-100': currentCategoryIndex === index
+        }"
+        :ref="setItemRef"
+        @click="onItemClick(index)"
       >
         {{ item.name }}
       </li>
@@ -22,13 +36,63 @@
 </template>
 
 <script setup>
-// vite 创建的项目可以直接使用 defineProps，不用导入
+import { useScroll } from '@vueuse/core'
+import { onBeforeUpdate, ref, watch } from 'vue'
 
+// vite 创建的项目可以直接使用 defineProps，不用导入
 defineProps({
   data: {
     type: Array,
     required: true
   }
 })
+
+// 滑块
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '60px'
+})
+
+/**
+ * 计算滑块的展示 宽度width 和 偏移量left
+ * 1、选中的 item 下标：currentCategoryIndex
+ * 2、所有 item 元素：itemRefs
+ * 3、ul 的横向滚动偏离位置：ulScrollLeft
+ * 4、最后在 currentCategoryIndex 发生改变时，获取 item 下标元素的 left 和 width，计算 sliderStyle 即可
+ */
+
+// 选中 item 的下标
+const currentCategoryIndex = ref(0)
+// 获取填充的所有 item 元素
+let itemRefs = []
+// 想获取动态dom元素的ref得用函数获取
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el)
+  }
+}
+// 数据更前后，dom 更新前，把获取的 dom 重置
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+
+// 获取 ul 元素，以计算偏移位置
+const ulTarget = ref(null)
+const { x: ulScrollLeft } = useScroll(ulTarget)
+watch(currentCategoryIndex, (val) => {
+  // 获取选中元素的 left、width
+  const { width, left } = itemRefs[val].getBoundingClientRect()
+  // 为 sliderStyle 设置样式
+  sliderStyle.value = {
+    // 滑块的位置 = ul 横向滚动位置 + 当前元素的 left 偏移量 - ul padding
+    transform: `translateX(${ulScrollLeft.value + left - 10}px)`,
+    width: `${width}px`
+  }
+})
+
+// item 点击事件
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
 </script>
 <style scoped></style>
