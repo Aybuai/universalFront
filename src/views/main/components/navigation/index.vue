dae6a9ef4121878a9dcbf071a2a11a22df1433dd
Aybuai <13624448268@163.com>
2025-07-18 12:01:29 +0800

    feat: pc navigation

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,10 +1,12 @@
 <template>
   <mobile-navigation-vue v-if="isMobileTerminal" />
+  <pc-navigation-vue v-else></pc-navigation-vue>
 </template>
 
 <script setup>
 import { useStore } from 'vuex'
 import mobileNavigationVue from './mobile/index.vue'
+import pcNavigationVue from './pc/index.vue'
 import { isMobileTerminal } from '@/utils/flexible'
 
 const store = useStore()
