21d83fca6a05dddfec155854c7d670cd624abe93
Aybuai <13624448268@163.com>
2025-07-19 13:57:25 +0800

    feat: 图片展示列表

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 
 <template>
   <!-- 一级路由出口 -->
-  <router-view />
+  <div class="h-screen fixed top-0 left-0 w-screen">
+    <router-view />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/views/main/components/list/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <item-vue v-for="item in pexelsList" :key="item.id" :data="item"></item-vue>
+  </div>
+</template>
+
+<script setup>
+import { getPexelsList } from '@/api/pexel'
+import { ref } from 'vue'
+import itemVue from './item.vue'
+
+/**
+ * 构建数据请求
+ */
+let query = {
+  page: 1,
+  size: 20
+}
+const pexelsList = ref([])
+const getPexelsData = async () => {
+  const { list } = await getPexelsList()
+  pexelsList.value = list
+}
+getPexelsData()
+</script>
+<style scoped lang="scss"></style>
