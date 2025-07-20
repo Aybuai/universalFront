2f23c24118cbae56acef31782c4b20a8c578b09e
Aybuai <13624448268@163.com>
2025-07-20 20:42:09 +0800

    feat: infinite 长列表

---
+++ src/libs/infinite/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <!-- 内容 -->
+    <slot />
+    <div ref="loadingTarget" class="h-6 py-4">
+      <!-- 加载更多 -->
+      <m-svg-icon
+        v-show="loading"
+        class="w-4 h-4 mx-auto animate-spin"
+        name="infinite-load"
+      ></m-svg-icon>
+      <!-- 没有更多数据了 -->
+      <p v-if="isFinished" class="text-center text-base text-zinc-400">
+        已经没有更多数据了！
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useIntersectionObserver, useVModel } from '@vueuse/core'
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+  // 是否处于加载状态
+  modelValue: {
+    type: Boolean,
+    required: true
+  },
+  // 数据是否全部加载完成
+  isFinished: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emits = defineEmits(['onLoad', 'update:modelValue'])
+
+// 处理 loading 状态
+const loading = useVModel(props)
+
+// 滚动的元素
+const loadingTarget = ref(null)
+// 记录当前是否在底部（是否交叉）
+const targetIsIntersecting = ref(false)
+
+// 利用 useIntersectionObserver 方法，监听元素可见行为，用于判断列表滚动到底部
+useIntersectionObserver(loadingTarget, ([{ isIntersecting }]) => {
+  targetIsIntersecting.value = isIntersecting
+  emitLoad()
+})
+
+/**
+ * 触发 load
+ */
+const emitLoad = () => {
+  // 当加载更多的视图可见时，且 loading 为 false，且数据没有全部加载完成
+  // 加载更多数据
+  if (targetIsIntersecting.value && !loading.value && !props.isFinished) {
+    // 修改加载数据标记
+    loading.value = true
+    // 触发加载更多行为
+    emits('onLoad')
+  }
+}
+
+/**
+ * 监听 loading 的变化，解决数据加载完成后，首屏未铺满的问题
+ */
+watch(loading, () => {
+  // 触发 load，延迟处理，等待渲染和 useIntersectionObserver 的再次触发
+  setTimeout(() => {
+    emitLoad()
+  }, 100)
+})
+</script>
+<style scoped lang="scss"></style>
--- src/views/main/components/list/index.vue
+++ src/views/main/components/list/index.vue
@@ -1,15 +1,21 @@
 <template>
-  <m-waterfall
-    :data="pexelsList"
-    nodeKey="id"
-    :column="isMobileTerminal ? 2 : 5"
-    :picturePreReading="false"
-    class="w-full px-1"
+  <m-infinite
+    v-model="loading"
+    :isFinished="isFinished"
+    @onLoad="getPexelsData"
   >
-    <template v-slot="{ item, width }">
-      <item-vue :data="item" :width="width"></item-vue>
-    </template>
-  </m-waterfall>
+    <m-waterfall
+      :data="pexelsList"
+      nodeKey="id"
+      :column="isMobileTerminal ? 2 : 5"
+      :picturePreReading="false"
+      class="w-full px-1"
+    >
+      <template v-slot="{ item, width }">
+        <item-vue :data="item" :width="width"></item-vue>
+      </template>
+    </m-waterfall>
+  </m-infinite>
 </template>
 
 <script setup>
@@ -25,11 +31,40 @@ let query = {
   page: 1,
   size: 20
 }
+// 数据是否在加载中
+const loading = ref(false)
+// 数据是否全部加载完成
+const isFinished = ref(false)
+// 数据源
 const pexelsList = ref([])
+/**
+ * 加载数据的方法
+ */
 const getPexelsData = async () => {
-  const { list } = await getPexelsList(query)
-  pexelsList.value = list
+  // 数据全部加载完成，不再请求
+  if (isFinished.value) return
+
+  // 完成第一次请求之后，后续再请求让 page 自增 1
+  if (pexelsList.value.length && pexelsList.value.length > 0) {
+    query.page += 1
+  }
+
+  // 触发接口请求
+  const { list, total } = await getPexelsList(query)
+
+  if (query.page === 1) {
+    pexelsList.value = list
+  } else {
+    pexelsList.value.push(...list)
+  }
+
+  // 判断数据是否全部加载完成
+  if (pexelsList.value.length === total) {
+    isFinished.value = true
+  }
+
+  // 修改 loading 标记
+  loading.value = false
 }
-getPexelsData()
 </script>
 <style scoped lang="scss"></style>
