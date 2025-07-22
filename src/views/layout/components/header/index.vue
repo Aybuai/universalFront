60aec0d0fcd057af2829d4bbde322846dc9d5881
Aybuai <13624448268@163.com>
2025-07-22 17:53:31 +0800

    feat: 引导功能 driver

--- src/views/layout/components/header/index.vue
+++ src/views/layout/components/header/index.vue
@@ -5,12 +5,12 @@
     <div class="flex items-center">
       <img
         v-lazy
-        class="h-4 cursor-pointer mr-2"
+        class="guide-home h-4 cursor-pointer mr-2"
         src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
         @click="onToHome"
       />
 
-      <header-search-vue class="mr-1"></header-search-vue>
+      <header-search-vue class="mr-1 guide-search"></header-search-vue>
       <header-theme-vue class="mr-1"></header-theme-vue>
       <header-my-vue></header-my-vue>
     </div>
